bc4b5fe76000a92de306fbaded330aa2ef075baa
Tarique <Tariqueelshamy11@gmail.com>
2025-06-18 11:01:58 -0400

    hide

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
   
 // controller actions
+const User = require('../models/User')
+
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
   }
@@ -9,9 +12,21 @@ module.exports.signup_get = (req, res) => {
   }
   
   module.exports.signup_post = async (req, res) => {
-    res.send('new signup');
+    const {email, password } = req.body;
+    try{
+         const user = await User.create({email, password});
+         res.status(201),json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('error, user wasnt created')
+    }
+  
   }
   
   module.exports.login_post = async (req, res) => {
+    const {email, password } = req.body;
+    
+    console.log(email, password)
     res.send('user login');
   }
