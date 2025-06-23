e78da71d3fe5f47ca729ca032200c30d04b08166
Tarique <Tariqueelshamy11@gmail.com>
2025-06-23 05:49:52 -0400

    hws 9 to 11 worked thru

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
@@ -17,7 +18,10 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-// routes
+
+//routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
