4af6ca660ae4faecde1515ead32d06b298364ab2
Tarique <Tariqueelshamy11@gmail.com>
2025-06-24 06:54:23 -0400

    13-15

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-require('dotenv').config()
+const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
+
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
+
 // view engine
 app.set('view engine', 'ejs');
 
@@ -19,9 +22,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 
-//routes
+
+// routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-// cookies
